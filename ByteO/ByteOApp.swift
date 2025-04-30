4fe2f367d1c892bc01d4b8894a86617026e31f9b
SOZx <monaaa.saad@yahoo.com>
2025-04-30 10:11:53 +0300

    Initial Commit

---
+++ ByteO/ByteOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteOApp.swift
+//  ByteO
+//
+//  Created by Mona on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ByteOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByteOTests/ByteOTests.swift
@@ -0,0 +1,17 @@
+//
+//  ByteOTests.swift
+//  ByteOTests
+//
+//  Created by Mona on 30/04/2025.
+//
+
+import Testing
+@testable import ByteO
+
+struct ByteOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
